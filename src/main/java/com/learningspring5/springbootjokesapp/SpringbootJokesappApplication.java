822cf9365b86375b5308b55b11879d2629114a12
mehj123 <mehjabeen.begum@outlook.com>
2019-05-23 11:08:46 +0400

    Spring Boot Jokes App - Changed Banner

---
+++ src/main/java/com/learningspring5/springbootjokesapp/SpringbootJokesappApplication.java
@@ -0,0 +1,13 @@
+package com.learningspring5.springbootjokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJokesappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJokesappApplication.class, args);
+    }
+
+}
